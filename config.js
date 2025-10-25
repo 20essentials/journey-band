017ea8252f8dd57b594261155625419538e4c193
DominicDev <s0l0estudi4r@gmail.com>
2025-10-25 12:32:58 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'JOURNEY ';
+export const nameSongs = [
+	"DON'T STOP BELIEVIN'", /*1*/
+	"SEPARATE WAYS ", /*2*/
+	"ANY WAY YOU WANT IT", /*3*/
+	"FAITHFULLY", /*4*/
+	"OPEN ARMS ", /*5*/
+	"WHEEL IN THE SKY", /*6*/
+	"LIGHTS", /*7*/
+	"LOVIN', TOUCHIN', SQUEEZIN'", /*8*/
+	"WHO'S CRYING NOW", /*9*/
+	"SEND HER MY LOVE", /*10*/
+	"STONE IN LOVE", /*11*/
+	"ONLY THE YOUNG", /*12*/
+	"WHEN YOU LOVE A WOMAN", /*13*/
+	"BE GOOD TO YOURSELF", /*14*/
+	"I'LL BE ALRIGHT WITHOUT YOU", /*15*/
+	"GIRL CAN'T HELP IT", /*16*/
+	"ASK THE LONELY", /*17*/
+	"AFTER THE FALL", /*18*/
+	"STILL THEY RIDE", /*19*/
+	"ESCAPE", /*20*/
+]
